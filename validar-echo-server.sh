105f872acf8654fe9a7ccb774a21fba34ea4a852
sofiajaves <83650724+sofiajaves@users.noreply.github.com>
2024-09-02 23:40:56 -0300

    fix for test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,7 +7,7 @@ RESPONSE=$(docker run --rm --network tp0_testing_net netcat-test)
 echo "Response from container: '$RESPONSE'"
 
 
-if [ "$RESPONSE" == "hello world" ]; then
+if [ "$RESPONSE" = "hello world" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
